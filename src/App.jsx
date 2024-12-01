d34024e07898c0c712dd5c0b4c5ef022443a3e15
nafihkk <muhammadnafihkk786@gmail.com>
2024-12-01 09:55:52 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from './Componants/Navbar'
 import NewsBoard from './Componants/NewsBoard'
+import './App.css'
 
 const App = () => {
   const [category,setCategory]= useState("general")
